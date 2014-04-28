f28f34f1d36efa99026dd2f6316aa29033b6f92d
csreid <csreid@purdue.edu>
2014-04-28 16:52:06 -0400

    compat generator finished

--- server/compat.c
+++ server/compat.c
@@ -69,56 +69,50 @@ int main(int argc, char ** argv){
     int values[numrows];
     int compat_table_size =(numrows*(numrows-1))/2 ;
     long compat_table[compat_table_size][3];
-    printf("Number of rows: %i\n", numrows);
-    printf("Table size: %i\n", numrows);
 
     for(i = 0; i < numrows; i++){
-        printf("fetching row...\n");
         rows[i] = mysql_fetch_row(results);
-        printf("fetched:");
-        for(j = 0; j < 14; j++){
-            printf("%s ", rows[i][j]);
-        }
-        printf("\n");
     }
-    printf("Generating compat table\n");
 
     i = 0;
-    j = numrows -1 ;
+    j = 0;
+    int row = 0;
 
-    while(i < j){
-        printf("thingy1: %s\n", rows[j][12]);
-        printf("thingy2: %s\n", rows[i][12]);
-        compat_table[i][0] = atol((char *)rows[j][12]);
-        compat_table[i][1] = atol((char *)rows[i][12]);
-        printf("table 1: %ld\n", compat_table[i][0]);
-        printf("table 2: %ld\n\n", compat_table[i][1]);
-        total = 0;
-        for(k = 4; k <= 6; k++){
-            total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10)));
-        }
-        if(!strcmp(rows[i][7], rows[j][7])){
-            //do nothing
-        }else{
-            total += 100;
-        } 
-        for(k = 8; k <= 11; k++){
-            if(k != 11){
-                total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10))); 
-            } else {
-                total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])/10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])/10))); 
+    for(i=0; i < numrows-1; i++){
+        for(j = i+1; j < numrows; j++){
+            compat_table[row][0] = atol((char *)rows[i][12]);
+            compat_table[row][1] = atol((char *)rows[j][12]);
+
+            printf("Comparing %ld to %ld\n\n", compat_table[i+j-1][0], compat_table[i+j-1][1]);
+            total = 0;
+            for(k = 4; k <= 6; k++){
+                total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10)));
             }
+            if(!strcmp(rows[i][7], rows[j][7])){
+                //do nothing
+            }else{
+                total += 100;
+            }
+
+            for(k = 8; k <= 11; k++){
+                if(k != 11){
+                    total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])*10))); 
+                } else {
+                    total += ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])/10) * ((str_to_bool(rows[i][k])-str_to_bool(rows[j][k])/10))); 
+                }
+            }
+            total += (atoi((rows[i][13]))-atoi(rows[j][13]))*2;
+            compat_table[row][2] = sqrt(total);
+            row ++;
         }
-        compat_table[i][2] = sqrt(total);
-        i++;
-        j--;
     }
 
-        printf("  s_id 1  |  s_id 2  | compat\n");
+        printf("\n  s_id 1  |  s_id 2  | compat\n");
         printf("----------+----------+----------\n");
     for(i = 0; i < compat_table_size; i++){
         printf("%ld|%ld|   %ld  \n", compat_table[i][0], compat_table[i][1], compat_table[i][2]);
         printf("----------+----------+------\n");
     }
     printf("Done!\n");
+    return compat_table;
 }
